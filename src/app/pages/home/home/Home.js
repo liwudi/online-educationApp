979fa0da55824fd2821dbfb5d09c7744f81af5a6
liwd <liwd@mapbar.com>
2018-01-31 19:33:46 +0800

    构建首页

--- src/app/pages/home/home/Home.js
+++ src/app/pages/home/home/Home.js
@@ -16,7 +16,42 @@ export default class Home extends Component{
                 <div className="swiper">
                     <img className="wrapper" src="https://intl.rakuten-static.com/b/gs/en/event/freeshipping/20180123/img/840x417cn.jpg" alt=""/>
                 </div>
-                首页
+
+                {/*视频区*/}
+                <div className="area-video">
+                    <div className="disFx space-between ">
+                        <img src={require('../../../assets/myicon/destinationed.png')} alt=""/>
+                        <span>更多视频>></span>
+                    </div>
+
+                </div>
+
+                {/*音频区*/}
+                <div className="area-audio">
+                    <div>
+                        <img src={require('../../../assets/myicon/destinationed.png')} alt=""/>
+                        <span>更多视频>></span>
+                    </div>
+
+                </div>
+
+                {/*会员专享*/}
+                <div className="area-member">
+                    <div>
+                        <img src={require('../../../assets/myicon/destinationed.png')} alt=""/>
+                        <span>更多视频>></span>
+                    </div>
+
+                </div>
+
+                {/*公司动态*/}
+                <div className="area-company">
+                    <div>
+                        <img src={require('../../../assets/myicon/destinationed.png')} alt=""/>
+                        <span>更多视频>></span>
+                    </div>
+
+                </div>
             </div>
         )
     }
