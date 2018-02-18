fb9926981efc4e56d7fe071ed0b09a77242aa6b7
liwd <liwd@mapbar.com>
2018-02-19 03:12:00 +0800

    完成轮播图

--- src/app/components/SearchComponent.js
+++ src/app/components/SearchComponent.js
@@ -6,13 +6,36 @@ import React, { Component } from 'react';
 
 export default class InputComponent extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            value: this.props.value
+        }
+    }
+    changeEvent(e){
+        var value = e.target.value;
+        this.setState({
+            value
+        })
+        this.props.getValue && this.props.getValue(value);
+    }
+    searchEvent(){
+        this.props.searchEvent && this.props.searchEvent();
     }
     render(){
         return (
-            <div className="search-container rowCenter border padding">
-                <img className="searchIcon" src={require('../assets/myicon/search.png')} alt=""/>
-                <input placeholder="请输入搜索内容" className="inputDefault" type="text"/>
+            <div className="search-container rowCenter border paddingSmall">
+                <img
+                    onClick={()=>this.searchEvent()}
+                    className="searchIcon"
+                    src={require('../assets/myicon/search.png')}
+                    alt=""/>
+                <input
+                    value={this.state.value}
+                    onChange={(e)=>this.changeEvent(e)}
+                    placeholder="请输入搜索内容"
+                    style={{backgroundColor:'#eeeeee'}}
+                    className="inputDefault marginLeft"
+                    type="text"/>
             </div>
         )
     }
--- src/app/pages/home/home/Home.js
+++ src/app/pages/home/home/Home.js
@@ -14,21 +14,77 @@ export default class Home extends Component{
         super(props);
         this.state = {
             src: 'http://img1.imgtn.bdimg.com/it/u=2364244149,3298797080&fm=27&gp=0.jpg',
-            videoList: [{},{},{},{}]
+            videoList: [{},{},{},{}],
+            searchContent: '',
+            bannerList: [
+                {
+                    img: "https://intl.rakuten-static.com/b/gs/en/event/freeshipping/20180123/img/840x417cn.jpg"
+                },{
+                    img: "https://m.360buyimg.com/img/jfs/t2977/335/1927273397/18081/64a032d5/5796df21N0c6edd4e.png"
+                },{
+                    img: "https://img30.360buyimg.com/da/s750x366_jfs/t11278/255/899926154/116991/f64c29a6/59fad637N96104620.jpg"
+                }
+            ],
+            bannerSrc: 'https://intl.rakuten-static.com/b/gs/en/event/freeshipping/20180123/img/840x417cn.jpg'
         }
     }
     goVedioDetail(){
         alert(1);
         this.props.history.push('/audio');
     }
+    getValue(value){
+        this.setState({
+            searchContent: value
+        })
+    }
+    searchEvent(){
+        console.log(this.state.searchContent);
+
+    }
+    componentDidMount(){
+        this.getBannerList();
+    }
+    componentWillUnMount(){
+        clearInterval(this.timer);
+    }
+    getBannerList(){
+        console.log('获取轮播列表的逻辑');
+        var that = this;
+        var banner = new Banner();
+        banner.init();
+        function Banner() {
+            this.index = 0;
+            this.init = function () {
+                that.timer = setInterval(()=>{
+                    this.index++;
+                    if(this.index > 2){
+                        this.index = 0
+                    }
+                    that.setState({
+                        bannerSrc: that.state.bannerList[this.index].img
+                    })
+                },1500);
+
+            }
+        }
+
+    }
     render(){
         return (
             <div className="fx1 autoBox">
                 <div className="center paddingTop paddingBottom">
-                    <SearchComponent/>
+                    <SearchComponent
+                        value={this.state.searchContent}
+                        getValue={(value)=>this.getValue(value)}
+                        searchEvent={()=>this.searchEvent()}
+                    />
                 </div>
                 <div className="height200">
-                    <img className="wrapper" src="https://intl.rakuten-static.com/b/gs/en/event/freeshipping/20180123/img/840x417cn.jpg" alt=""/>
+                    <img
+                        className="wrapper"
+                        src={this.state.bannerSrc}
+                        alt=""
+                    />
                 </div>
 
                 {/*视频区*/}
